726a43e2de604a9b9c8f9f944942316fecbc4580
Darius Cupsa <cupsadarius@gmail.com>
2026-09-11 20:26:53 +0300

    chore: initialize Relay macOS app

---
+++ Relay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Relay
+//
+//  Created by Darius Cupsa on 11/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Relay/RelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelayApp.swift
+//  Relay
+//
+//  Created by Darius Cupsa on 11/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct RelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
